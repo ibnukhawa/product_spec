90df6e7c19f95f2195598db1141d925d8ebb7a24
ibnukhawa <firstibnu@gmail.com>
2021-06-04 13:55:23 +0700

    adding structure

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+  'name': 'Product Specification',
+  'author': 'Ibnu Nur Khawarizmi',
+  'version': '0.1',
+  'depends': [
+    'sale',
+  ],
+  'data': [
+    'views/views.xml',
+  ],
+  'qweb': [
+    
+  ],
+  'sequence': 1,
+  'auto_install': False,
+  'installable': True,
+  'application': True,
+  'category': 'Sale',
+  'summary': 'Add Specification on product notebook',
+  'license': 'OPL-1',
+  'website': 'https://www.github.com/ibnukhawa',
+  'description': '-'
+}
